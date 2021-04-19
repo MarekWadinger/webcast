c4902b277ac77baf81a73ea2145416a22bceed16
Marek Wadinger <50716630+MarekWadinger@users.noreply.github.com>
2021-04-19 21:12:09 +0200

    Update forecast_streamlit.py

--- forecast_streamlit.py
+++ forecast_streamlit.py
@@ -21,9 +21,22 @@ def prediction(data_file, prediction_period):
     future = m.make_future_dataframe(periods=prediction_period * 24 * 4 , freq='15min')
     # Make prediction
     forecast = m.predict(future)
-
     
     return m, forecast
+
+def save_csv(forecast):
+    csv = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="load_forecast.csv" target="_blank">Download csv file</a>'
+    
+    return href
+
+def save_json(forecast):
+    json = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json().encode()
+    b64 = base64.b64encode(json).decode()
+    href = f'<a href="data:file/json;base64,{b64}" download="load_forecast.json" target="_blank">Download json file</a>'
+
+    return href
 
 # Build web app
 # -----------------------------------------------------------------------------
@@ -34,23 +47,13 @@ file = st.file_uploader("Pick a file")
 # user_date = user_date.strftime("%Y-%m-%d")
 # st.text(user_date)
 user_periods = st.slider("Prediction period in days:", 1, 7, 1)
-#user_plot = st.button('Plot forecast')
-user_save_json = st.button('Save as json')
-user_save_csv = st.button('Save as csv')
 
 if file:
     # Import the csv file as pandas data frame
     m, forecast = prediction(file, user_periods)
-    #if user_plot:
     
-    if user_save_json:
-        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json(str(user_periods) +'_day_load_forecast.json')
-    if user_save_csv:
-        csv = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_csv(str(user_periods) +'_day_load_forecast.csv').encode()
-        b64 = base64.b64encode(csv).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="captura.csv" target="_blank">Download csv file</a>'
-        st.markdown(href, unsafe_allow_html=True)
+    st.markdown(save_json(forecast), unsafe_allow_html=True)   
+    st.markdown(save_csv(forecast), unsafe_allow_html=True)
+    
     st.plotly_chart(plot_plotly(m, forecast))
     st.plotly_chart(plot_components_plotly(m, forecast))
-        
-        
