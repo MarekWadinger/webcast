7337b55dad725512735a006ceca5d2de8818169f
Marek Wadinger <marekwadinger5506@gmail.com>
2021-05-17 23:59:21 +0200

    Solved functions shadowing the names of outer scope

---
+++ src/anomaly.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import time
+
+from pyod.models.iforest import IForest
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+def resample_data(s, ratio=0.9):
+    tf = s['ds'].diff().value_counts()
+
+    counter = 0
+    for i in range(len(tf)):
+        counter += tf[i]
+        if counter / sum(tf[:]) >= ratio:
+            break
+
+    if tf.index[i].days == 0:
+        freq = str(tf.index[i].seconds) + "S"
+    else:
+        freq = str(tf.index[i].days) + "D"
+
+    s = s.set_index('ds').resample(freq).first().reset_index()
+    s = s.dropna()
+
+    return s, freq
+
+
+def anomaly_rate(model, validation_df, freq, plot=False):
+    if freq[:-1].isnumeric() and (freq[-1] == 'S' or freq[-1] == 'D'):
+        last_history = (model.start + model.t_scale).round(freq)
+    else:
+        raise ValueError("Unsupported frequency format. Provide any valid frequency for pd.date_range, as multiple of 'D' or 'S'.")
+
+    first_validation = validation_df['ds'].iloc[0]
+    last_validation = validation_df['ds'].iloc[-1]
+
+    if last_validation > last_history:
+        if first_validation <= last_history:
+            validation_df = validation_df.loc[validation_df['ds'] > last_history].dropna()[['ds', 'y']]
+
+        start_timer = time.time()
+        future = validation_df['ds'].to_frame(name='ds')
+        prediction_data = model.predict(future)[['ds', 'yhat']]     # TOO SLOW!
+        print("--- Prediction: %s seconds ---" % (time.time() - start_timer))
+
+        df = pd.DataFrame({'y': validation_df['y'].values, 'yhat': prediction_data['yhat'].values})
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        df[['y', 'yhat']] = scaler.fit_transform(df[['y', 'yhat']])
+
+        clf_name = 'iForest'
+        clf = IForest()
+        clf.fit(df)
+
+        # get the prediction labels and outlier scores of the training data
+        y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
+        y_train_scores = clf.decision_scores_  # raw outlier scores
+
+        if plot:
+            # fig = plt.figure(facecolor='w', figsize=(10, 6))
+            # ax = fig.add_subplot(111)
+            # ax.plot(prediction_data['ds'].dt.to_pydatetime(), deviation, 'k.')
+            # ax.plot(prediction_data['ds'][y_train_pred == 1].dt.to_pydatetime(), deviation[y_train_pred == 1], 'r.')
+            # fig.show()
+
+            fig1 = plt.figure(facecolor='w', figsize=(10, 6))
+            ax = fig1.add_subplot(111)
+            ax.plot(prediction_data['ds'].dt.to_pydatetime(), y_train_scores)
+            ax.plot(prediction_data['ds'][y_train_pred == 1].dt.to_pydatetime(), y_train_scores[y_train_pred == 1], 'r.')
+            fig1.show()
+
+            fig2 = plt.figure(facecolor='w', figsize=(10, 6))
+            ax = fig2.add_subplot(111)
+            ax.plot(validation_df['ds'].dt.to_pydatetime(), validation_df['y'].values)
+            ax.plot(prediction_data['ds'].dt.to_pydatetime(), prediction_data['yhat'].values)
+            ax.vlines(prediction_data['ds'][y_train_pred == 1].dt.to_pydatetime(), min(validation_df['y'].values), max(validation_df['y'].values), 'r')
+            fig2.show()
+
+        return sum(y_train_pred) / len(y_train_pred)
+
+    else:
+        raise ValueError("Validation dataset has no data point after last member of time-series of historical data that",
+                         "the model was trained on. Please use validation dataset with last member of the time series",
+                         "after %s." % last_history)
---
+++ src/forecast_streamlit.py
@@ -0,0 +1,138 @@
+# streamlit run C:\Users\marek\PycharmProjects\pythonProject\Load_forecast_streamlit.py
+import streamlit as st
+import pandas as pd
+import base64
+import json
+import time
+
+from prophet.serialize import model_to_json
+from prophet.plot import plot_plotly, plot_components_plotly
+
+from loadforecast.forecaster import LoadProphet
+from loadforecast.model_load import model_load
+from anomaly import resample_data, anomaly_rate
+
+
+@st.cache(suppress_st_warning=True)
+def modify_data(csv_data_file):
+    df = pd.read_csv(csv_data_file)
+    df_new = df[['DateTime', 'Load']].rename(columns={'DateTime': 'ds', 'Load': 'y'})
+    df_new['ds'] = pd.to_datetime(df_new['ds'])
+    return df_new
+
+
+@st.cache(suppress_st_warning=True)
+def build_model(df, pre_model, country, chps, sps, hps, ds, ws, ys):
+    model = LoadProphet(df, pretrained_model=pre_model, country=country, changepoint_prior_scale=chps,
+                        seasonality_prior_scale=sps, holidays_prior_scale=hps, daily_seasonality=ds,
+                        weekly_seasonality=ws, yearly_seasonality=ys)
+    return model
+
+
+@st.cache(suppress_st_warning=True)
+def predict_future(model, period):
+    return model.prediction(prediction_periods=period*24*4, frequency='15min')
+
+
+def save_csv(df_forecast):
+    csv = df_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="load_forecast.csv" target="_blank">Download csv file</a>'
+    return href
+
+
+def save_json(df_forecast):
+    json_file = df_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json().encode()
+    b64 = base64.b64encode(json_file).decode()
+    href = f'<a href="data:file/json;base64,{b64}" download="load_forecast.json" target="_blank">Download json file</a>'
+    return href
+
+
+def model_json(model):
+    json_file = model_to_json(model)
+    json_file = json.dumps(json_file)
+    b64 = base64.b64encode(json_file.encode()).decode()
+    href = f'<a href="data:file/json;base64,{b64}" download="loadForecast_model.json" ' \
+           f'target="_blank">Download model</a>'
+    return href
+
+
+# Build web app
+# -----------------------------------------------------------------------------
+st.title("""Electrical load forecast 
+Using  *fbprophet*""")
+col1, col2 = st.beta_columns(2)
+with col1:
+    data_file = st.file_uploader('Import data as csv file. Data should have two columns named DateTime and Load.')
+with col2:
+    model_file = st.file_uploader('Import pretrained model as json file.')
+user_period = st.slider('Prediction period in days:', min_value=1, max_value=14, value=1, step=1, )
+user_run = st.button('Run prediction')
+user_plot = st.button('Plot forecast')
+p1 = st.empty()
+p2 = st.empty()
+p3 = st.empty()
+
+# Build Sidebar
+with st.sidebar:
+    st.title('Customize model')
+    user_country = st.text_input('Insert ISO code of Country', value='BE')
+    st.text('Take local holidays into account when training a model.')
+    st.caption('List of available ISO codes here:')
+    st.caption('https://github.com/dr-prodigy/python-holidays')
+    st.subheader('Change with caution:')
+    with st.form(key='Model Parameters'):
+        user_chps = st.slider('Changepoint Prior Scale', min_value=0.001, max_value=0.050, value=0.001, step=0.001, format='%.3f')
+        user_sps = st.slider('Seasonality Prior Scale', min_value=0.01, max_value=10.00, value=10.00, step=0.01)
+        user_hps = st.slider('Holidays Prior Scale', min_value=0.01, max_value=10.00, value=0.1, step=0.01)
+        user_ds = st.selectbox('Daily Seasonality', [True, False, 'auto'], index=0)
+        user_ws = st.selectbox('Weekly Seasonality', [True, False, 'auto', 'Custom'], index=3)
+        st.caption('To use "Custom" Weekly Seasonality submit "Custom" and then change value')
+        weekly_placeholder = st.empty()
+        user_ys = st.selectbox('Yearly Seasonality', [True, False, 'auto'], index=1)
+        submit_button = st.form_submit_button(label='Submit')
+        if user_ws == 'Custom':
+            user_ws = weekly_placeholder.slider('Weekly seasonality', min_value=0, max_value=100, value=28, step=2)
+
+if data_file:
+    data_frame = modify_data(data_file)
+
+    if model_file:
+        attr_dict = json.loads(json.load(model_file))
+        m = model_load(attr_dict)
+        df_resampled, frequency = resample_data(data_frame)
+        anomaly_proportion = anomaly_rate(m, df_resampled, frequency, plot=False)
+        p1.info('Anomaly proportion between prediction and validation data is **%.2f %%**' % (anomaly_proportion*100))
+        if anomaly_proportion > 0.1:
+            p2.info('Model is outdated. Warm fitting new model...')
+            start_time = time.time()
+            m = build_model(data_frame, m, user_country, user_chps, user_sps, user_hps, user_ds, user_ws, user_ys)
+            fitting_time = time.time() - start_time
+        else:
+            p2.info('Model is up-to-date and ready for prediction.')
+            fitting_time = 0
+    else:
+        p2.info('Fitting new model...')
+        start_time = time.time()
+        m = build_model(data_frame, None, user_country, user_chps, user_sps, user_hps, user_ds, user_ws, user_ys)
+        fitting_time = time.time() - start_time
+
+    p3.info('Making prediction...')
+    start_time = time.time()
+    forecast = predict_future(m, user_period)
+    prediction_time = time.time() - start_time
+    p3.success('Prediction made with success. Fitting time: **%.2f s**. Prediction time: **%.2f s**.' % (fitting_time, prediction_time))
+
+    st.subheader('Download prediction')
+    st.markdown(save_json(forecast), unsafe_allow_html=True)
+    st.markdown(save_csv(forecast), unsafe_allow_html=True)
+    st.markdown(model_json(m), unsafe_allow_html=True)
+
+if user_plot:
+    st.plotly_chart(plot_plotly(m, forecast))
+    st.plotly_chart(plot_components_plotly(m, forecast))
+
+time.sleep(1.2)
+p1.empty()
+time.sleep(0.95)
+p2.empty()
