2c4875a80a3db571917b016bb92f0e2d6f1e23ec
marekwadinger <marekwadinger5506@gmail.com>
2023-10-26 13:42:14 +0200

    REMOVE: unnecessary files

--- anomaly_panels.py
+++
@@ -1,7 +0,0 @@
-from src import se_api_process
-from src.anomaly_report import tidy_individual_detection
-
-data = se_api_process.get_se_as_df('se_daily.json')
-data = data.drop(data.iloc[26].name)
-
-df_modules, yield_modules, df_outliers, df_scores = tidy_individual_detection(data, plot=True, report=True)
